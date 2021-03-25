cd6a1227ec2a558403af0eafe7646005689e79f3
seb45tian <sebastian.potthoff@gmail.com>
2021-03-25 21:24:20 +0100

    always return metric value even if 0 and get rid of err state as is no longer part of the SLURM states

--- partitions.go
+++ partitions.go
@@ -64,7 +64,6 @@ type PartitionMetrics struct {
 	ncomp  float64
 	ndown  float64
 	ndrain float64
-	nerr   float64
 	nfail  float64
 	nidle  float64
 	nmaint float64
@@ -74,7 +73,6 @@ type PartitionMetrics struct {
 	ccomp  float64
 	cdown  float64
 	cdrain float64
-	cerr   float64
 	cfail  float64
 	cidle  float64
 	cmaint float64
@@ -108,7 +106,6 @@ func ParsePartitionsMetrics() map[string]*PartitionMetrics {
             down := regexp.MustCompile(`^down`)
             drain := regexp.MustCompile(`^drain`)
             fail := regexp.MustCompile(`^fail`)
-            err := regexp.MustCompile(`^err`)
             idle := regexp.MustCompile(`^idle`)
             maint := regexp.MustCompile(`^maint`)
             mix := regexp.MustCompile(`^mix`)
@@ -129,9 +126,6 @@ func ParsePartitionsMetrics() map[string]*PartitionMetrics {
             case fail.MatchString(state) == true:
                 partitions[partition].nfail += node_count
                 partitions[partition].cfail += cpu_count
-            case err.MatchString(state) == true:
-                partitions[partition].nerr += node_count
-                partitions[partition].cerr += cpu_count
             case idle.MatchString(state) == true:
                 partitions[partition].nidle += node_count
                 partitions[partition].cidle += cpu_count
@@ -167,7 +161,6 @@ type PartitionsCollector struct {
 	ncomp  *prometheus.Desc
 	ndown  *prometheus.Desc
 	ndrain *prometheus.Desc
-	nerr   *prometheus.Desc
 	nfail  *prometheus.Desc
 	nidle  *prometheus.Desc
 	nmaint *prometheus.Desc
@@ -177,7 +170,6 @@ type PartitionsCollector struct {
 	ccomp  *prometheus.Desc
 	cdown  *prometheus.Desc
 	cdrain *prometheus.Desc
-	cerr   *prometheus.Desc
 	cfail  *prometheus.Desc
 	cidle  *prometheus.Desc
 	cmaint *prometheus.Desc
@@ -196,7 +188,6 @@ func NewPartitionsCollector() *PartitionsCollector {
 		ncomp:  prometheus.NewDesc("slurm_partition_nodes_comp", "Completing nodes for partition", labels, nil),
 		ndown:  prometheus.NewDesc("slurm_partition_nodes_down", "Down nodes for partition", labels, nil),
 		ndrain: prometheus.NewDesc("slurm_partition_nodes_drain", "Drain nodes for partition", labels, nil),
-		nerr:   prometheus.NewDesc("slurm_partition_nodes_err", "Error nodes for partition", labels, nil),
 		nfail:  prometheus.NewDesc("slurm_partition_nodes_fail", "Fail nodes for partition", labels, nil),
 		nidle:  prometheus.NewDesc("slurm_partition_nodes_idle", "Idle nodes for partition", labels, nil),
 		nmaint: prometheus.NewDesc("slurm_partition_nodes_maint", "Maint nodes for partition", labels, nil),
@@ -206,7 +197,6 @@ func NewPartitionsCollector() *PartitionsCollector {
 		ccomp:  prometheus.NewDesc("slurm_partition_cpus_comp", "Completing CPUs for partition", labels, nil),
 		cdown:  prometheus.NewDesc("slurm_partition_cpus_down", "Down CPUs for partition", labels, nil),
 		cdrain: prometheus.NewDesc("slurm_partition_cpus_drain", "Drain CPUs for partition", labels, nil),
-		cerr:   prometheus.NewDesc("slurm_partition_cpus_err", "Error CPUs for partition", labels, nil),
 		cfail:  prometheus.NewDesc("slurm_partition_cpus_fail", "Fail CPUs for partition", labels, nil),
 		cidle:  prometheus.NewDesc("slurm_partition_cpus_idle", "Idle CPUs for partition", labels, nil),
 		cmaint: prometheus.NewDesc("slurm_partition_cpus_maint", "Maint CPUs for partition", labels, nil),
@@ -221,7 +211,6 @@ func (pc *PartitionsCollector) Describe(ch chan<- *prometheus.Desc) {
 	ch <- pc.ncomp
 	ch <- pc.ndown
 	ch <- pc.ndrain
-	ch <- pc.nerr
 	ch <- pc.nfail
 	ch <- pc.nidle
 	ch <- pc.nmaint
@@ -231,7 +220,6 @@ func (pc *PartitionsCollector) Describe(ch chan<- *prometheus.Desc) {
 	ch <- pc.ccomp
 	ch <- pc.cdown
 	ch <- pc.cdrain
-	ch <- pc.cerr
 	ch <- pc.cfail
 	ch <- pc.cidle
 	ch <- pc.cmaint
@@ -242,68 +230,24 @@ func (pc *PartitionsCollector) Describe(ch chan<- *prometheus.Desc) {
 func (pc *PartitionsCollector) Collect(ch chan<- prometheus.Metric) {
     pm := ParsePartitionsMetrics()
     for p := range pm {
-        if pm[p].pending > 0 {
-            ch <- prometheus.MustNewConstMetric(pc.pending, prometheus.GaugeValue, pm[p].pending, p)
-        }
-        if pm[p].nalloc > 0 {
-            ch <- prometheus.MustNewConstMetric(pc.nalloc, prometheus.GaugeValue, pm[p].nalloc, p)
-        }
-        if pm[p].ncomp > 0 {
-            ch <- prometheus.MustNewConstMetric(pc.ncomp,  prometheus.GaugeValue, pm[p].ncomp, p)
-        }
-        if pm[p].ndown > 0 {
-            ch <- prometheus.MustNewConstMetric(pc.ndown,  prometheus.GaugeValue, pm[p].ndown, p)
-        }
-        if pm[p].ndrain > 0 {
-            ch <- prometheus.MustNewConstMetric(pc.ndrain, prometheus.GaugeValue, pm[p].ndrain, p)
-        }
-        if pm[p].nerr > 0 {
-            ch <- prometheus.MustNewConstMetric(pc.nerr,   prometheus.GaugeValue, pm[p].nerr, p)
-        }
-        if pm[p].nfail > 0 {
-            ch <- prometheus.MustNewConstMetric(pc.nfail,  prometheus.GaugeValue, pm[p].nfail, p)
-        }
-        if pm[p].nidle > 0 {
-            ch <- prometheus.MustNewConstMetric(pc.nidle,  prometheus.GaugeValue, pm[p].nidle, p)
-        }
-        if pm[p].nmaint > 0 {
-            ch <- prometheus.MustNewConstMetric(pc.nmaint, prometheus.GaugeValue, pm[p].nmaint, p)
-        }
-        if pm[p].nmix > 0 {
-            ch <- prometheus.MustNewConstMetric(pc.nmix,   prometheus.GaugeValue, pm[p].nmix, p)
-        }
-        if pm[p].nresv > 0 {
-            ch <- prometheus.MustNewConstMetric(pc.nresv,  prometheus.GaugeValue, pm[p].nresv, p)
-        }
-        if pm[p].calloc > 0 {
-            ch <- prometheus.MustNewConstMetric(pc.calloc, prometheus.GaugeValue, pm[p].calloc, p)
-        }
-        if pm[p].ccomp > 0 {
-            ch <- prometheus.MustNewConstMetric(pc.ccomp,  prometheus.GaugeValue, pm[p].ccomp, p)
-        }
-        if pm[p].cdown > 0 {
-            ch <- prometheus.MustNewConstMetric(pc.cdown,  prometheus.GaugeValue, pm[p].cdown, p)
-        }
-        if pm[p].cdrain > 0 {
-            ch <- prometheus.MustNewConstMetric(pc.cdrain, prometheus.GaugeValue, pm[p].cdrain, p)
-        }
-        if pm[p].cerr > 0 {
-            ch <- prometheus.MustNewConstMetric(pc.cerr,   prometheus.GaugeValue, pm[p].cerr, p)
-        }
-        if pm[p].cfail > 0 {
-            ch <- prometheus.MustNewConstMetric(pc.cfail,  prometheus.GaugeValue, pm[p].cfail, p)
-        }
-        if pm[p].cidle > 0 {
-            ch <- prometheus.MustNewConstMetric(pc.cidle,  prometheus.GaugeValue, pm[p].cidle, p)
-        }
-        if pm[p].cmaint > 0 {
-            ch <- prometheus.MustNewConstMetric(pc.cmaint, prometheus.GaugeValue, pm[p].cmaint, p)
-        }
-        if pm[p].cmix > 0 {
-            ch <- prometheus.MustNewConstMetric(pc.cmix,   prometheus.GaugeValue, pm[p].cmix, p)
-        }
-        if pm[p].cresv > 0 {
-            ch <- prometheus.MustNewConstMetric(pc.cresv,  prometheus.GaugeValue, pm[p].cresv, p)
-        }
+        ch <- prometheus.MustNewConstMetric(pc.pending, prometheus.GaugeValue, pm[p].pending, p)
+        ch <- prometheus.MustNewConstMetric(pc.nalloc, prometheus.GaugeValue, pm[p].nalloc, p)
+        ch <- prometheus.MustNewConstMetric(pc.ncomp,  prometheus.GaugeValue, pm[p].ncomp, p)
+        ch <- prometheus.MustNewConstMetric(pc.ndown,  prometheus.GaugeValue, pm[p].ndown, p)
+        ch <- prometheus.MustNewConstMetric(pc.ndrain, prometheus.GaugeValue, pm[p].ndrain, p)
+        ch <- prometheus.MustNewConstMetric(pc.nfail,  prometheus.GaugeValue, pm[p].nfail, p)
+        ch <- prometheus.MustNewConstMetric(pc.nidle,  prometheus.GaugeValue, pm[p].nidle, p)
+        ch <- prometheus.MustNewConstMetric(pc.nmaint, prometheus.GaugeValue, pm[p].nmaint, p)
+        ch <- prometheus.MustNewConstMetric(pc.nmix,   prometheus.GaugeValue, pm[p].nmix, p)
+        ch <- prometheus.MustNewConstMetric(pc.nresv,  prometheus.GaugeValue, pm[p].nresv, p)
+        ch <- prometheus.MustNewConstMetric(pc.calloc, prometheus.GaugeValue, pm[p].calloc, p)
+        ch <- prometheus.MustNewConstMetric(pc.ccomp,  prometheus.GaugeValue, pm[p].ccomp, p)
+        ch <- prometheus.MustNewConstMetric(pc.cdown,  prometheus.GaugeValue, pm[p].cdown, p)
+        ch <- prometheus.MustNewConstMetric(pc.cdrain, prometheus.GaugeValue, pm[p].cdrain, p)
+        ch <- prometheus.MustNewConstMetric(pc.cfail,  prometheus.GaugeValue, pm[p].cfail, p)
+        ch <- prometheus.MustNewConstMetric(pc.cidle,  prometheus.GaugeValue, pm[p].cidle, p)
+        ch <- prometheus.MustNewConstMetric(pc.cmaint, prometheus.GaugeValue, pm[p].cmaint, p)
+        ch <- prometheus.MustNewConstMetric(pc.cmix,   prometheus.GaugeValue, pm[p].cmix, p)
+        ch <- prometheus.MustNewConstMetric(pc.cresv,  prometheus.GaugeValue, pm[p].cresv, p)
     }
 }
